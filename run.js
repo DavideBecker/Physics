cf0951c40212b61a51f7eaa99edf6363029fc9e9
Davide Becker <davide.becker@stud.h-da.de>
2017-02-01 17:48:59 +0100

    Improve development node server

--- run.js
+++ run.js
@@ -1,14 +1,41 @@
 var express = require('express');
+var colors = require('colors');
+var argv = require('optimist').argv;
+var portfinder = require('portfinder');
 
+var port = argv.p;
+var logger = argv.l;
+var log = console.log;
 var app = express();
+
+if(!argv.p) {
+    portfinder.basePort = 8080;
+    portfinder.getPort(function(err, port) {
+        if(err) {
+            throw err
+        }
+
+        listen(port);
+    });
+} else {
+        listen(port);
+}
+
+function listen(port) {
+
+    app.use(express.static('app'));
+    var server = app.listen(port);
+
+    log('Starting up Server, serving '.yellow
+        + __dirname.green
+        + ' on port: '.yellow
+        + port.toString().cyan);
+
+    log('Hit CTRL-C to stop the server');
+
+}
 
 app.use(function(req, res, next) {
     console.log(req.method + ' ' + req.path);
     next();
 });
-
-app.use(express.static('app'));
-
-var server = app.listen(8000);
-
-console.log("Server running at http://localhost:8000");

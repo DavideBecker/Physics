fb4bf1ad3ac198f964948705b08c1b33d31ef01f
Davide Becker <davide.becker@stud.h-da.de>
2017-02-04 19:07:58 +0100

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -218,7 +218,7 @@ module.exports = {
         ],
         "radix": "error",
         "require-await": "error",
-        "require-jsdoc": "warn",
+        "require-jsdoc": 0,
         "require-yield": 0,
         "rest-spread-spacing": "error",
         "semi": "off",
